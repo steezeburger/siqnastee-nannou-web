9264f04a38e69ffe23f842bc713d4a1bb3e69e13
Jesse Snyder <jessetsnyder@gmail.com>
2023-02-14 20:11:58 -0700

    browser to rust communication

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
-import("../pkg/index.js").then(module =>{
-   module.main_web();
-});
+import('../pkg/index.js').then(module => {
+  let width = window.innerWidth
+  let height = window.innerHeight
+  module.print_wh(width, height)
+
+  module.main_web()
+
+  window.addEventListener('resize', function () {
+    let width = window.innerWidth
+    let height = window.innerHeight
+    module.print_wh(width, height)
+  })
+})
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,16 @@
+use async_std::task::block_on;
 use wasm_bindgen::prelude::wasm_bindgen;
 
+use sketch::{Model, run_app};
+
 mod sketch;
-use async_std::task::block_on;
-use sketch::{run_app, Model};
+// struct BrowserWindow {
+//     /// Width of the browser window
+//     width: u64,
+//
+//     /// Height of the browser window
+//     height: u64
+// }
 
 // web app entry_point
 #[wasm_bindgen]
@@ -14,4 +22,9 @@ pub async fn main_web() {
     block_on(async {
         run_app(model).await;
     });
+}
+
+#[wasm_bindgen]
+pub async fn print_wh(width: u32, height: u32) {
+    web_sys::console::log_1(&format!("w: {}, h: {}", width, height).into());
 }
