ab7e0537ae11ff8729af8e6e88eb0cf217c6fdb4
Jesse Snyder <jessetsnyder@gmail.com>
2023-02-19 22:33:00 -0700

    cleanup

--- js/index.js
+++ js/index.js
@@ -3,10 +3,4 @@ import('../pkg/index.js').then(module => {
   let height = window.innerHeight
 
   module.main_web(width, height)
-
-  // window.addEventListener('resize', function () {
-  //   let width = window.innerWidth
-  //   let height = window.innerHeight
-  //   module.print_wh(width, height)
-  // })
 })
